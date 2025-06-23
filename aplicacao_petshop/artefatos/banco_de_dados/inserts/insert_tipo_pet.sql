e33908698f31943486c03c75c6e6c54619c5c64f
@luisfl04_ <luisfelipecontato08@gmail.com>
2025-06-23 15:43:20 -0300

    push

---
+++ aplicacao_petshop/artefatos/banco_de_dados/inserts/insert_tipo_pet.sql
@@ -0,0 +1,8 @@
+-- Tipos de pets relacionados à aplicação:
+insert into tipo_pet(nome_tipo)
+values
+('cachorro'),
+('gato'),
+('cavalo'),
+('passarinho'),
+('gado');
---
+++ aplicacao_petshop/artefatos/banco_de_dados/inserts/insert_veterinario.sql
@@ -0,0 +1,15 @@
+-- Inserindo valores relacionados aos endereços dos veterinários, primeiramente: 
+INSERT INTO endereco (
+    rua, bairro, cidade, estado, complemento
+) VALUES
+    ('Rua das Flores', 'Jardim Primavera', 'São Paulo', 'São Paulo', 'Apto 12'),
+    ('Avenida dos Pássaros', 'Vila Nova', 'Rio de Janeiro', 'Rio de Janeiro', 'Casa 3'),
+    ('Travessa do Sol', 'Centro', 'Belo Horizonte', 'Minas Gerais', 'Próximo à Praça Central');
+
+-- Agora, inserindo os veterinários:
+INSERT INTO veterinario (
+    endereco_id, usuario, senha, numero_crmv, nome, telefone, cpf, data_nascimento, sexo
+) VALUES
+    ('1', 'anasilva321', 'ana321', 'CRMV1', 'Ana Silva', '11987654321', '12345678901', '1990-05-15', 'F'),
+    ('2', 'brunosantos432', 'bruna432', 'CRMV2', 'Bruna Santos', '21998765432', '98765432109', '1988-08-22', 'F'),
+    ('3', 'carlosalmeida321', 'carlos321', 'CRMV3', 'Carlos Almeida', '31987654321', '11223344556', '1985-03-10', 'M');
